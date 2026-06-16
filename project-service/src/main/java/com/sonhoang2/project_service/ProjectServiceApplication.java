efe53d51b2c61b6545b3e23747585dacc3e3d1eb
sonhoang2 <naruto3285@gmail.com>
2026-06-17 00:11:12 +0700

    feat: add JWT expiration property and enable Feign clients for project service

--- project-service/src/main/java/com/sonhoang2/project_service/ProjectServiceApplication.java
+++ project-service/src/main/java/com/sonhoang2/project_service/ProjectServiceApplication.java
@@ -2,7 +2,9 @@ package com.sonhoang2.project_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.sonhoang2.project_service.project.feign")
 @SpringBootApplication
 public class ProjectServiceApplication {
 
